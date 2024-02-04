1b19a57725a0bac632e734d595e546ff66820604
Zarif Karim <zarifkarim09@gmail.com>
2024-02-04 21:22:39 +1100

    configure jest

---
+++ src/routes/__tests__/healthcheck.test.ts
@@ -0,0 +1,19 @@
+import server from '../../app';
+import request from 'supertest';
+
+afterEach((done) => {
+    server.close();
+    done();
+});
+
+describe('Healthcheck', () => {
+    it('should return 200', async () => {
+        const response = await request(server).get('/healthcheck');
+        expect(response.status).toEqual(200);
+    });
+
+    it('should return ok in the body', async () => {
+        const response = await request(server).get('/healthcheck');
+        expect(response.text).toEqual('ok');
+    });
+});

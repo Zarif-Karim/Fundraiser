6faf8255052e6d6cd6fd80305c199377f649615b
Zarif Karim <zarifkarim09@gmail.com>
2024-02-04 23:29:09 +1100

    add dummy endpoint for storage

---
+++ src/routes/__tests__/storage.test.ts
@@ -0,0 +1,18 @@
+import server from "../../app";
+import request from "supertest";
+
+afterEach((done) => {
+  server.close();
+  done();
+});
+
+describe("Storage", () => {
+  it.each(["/storage", "/storage/healthcheck"])(
+    "should return 200 with ok on heartbeat - %s",
+    async (link) => {
+      const response = await request(server).get(link);
+      expect(response.status).toEqual(200);
+      expect(response.text).toEqual("ok");
+    }
+  );
+});
---
+++ src/routes/storage.ts
@@ -0,0 +1,9 @@
+import Router from "koa-router";
+
+const router = new Router();
+
+router.get(["/", "/healthcheck"], async (ctx) => {
+    ctx.body = "ok";
+});
+
+export default router;

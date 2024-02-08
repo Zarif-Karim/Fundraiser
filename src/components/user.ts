b3e0fd1d90c028430a7eddaab4319582848bce9c
Zarif Karim <zarifkarim09@gmail.com>
2024-02-09 02:00:50 +1100

    refactor user component

--- src/components/user.ts
+++ src/components/user.ts
@@ -8,18 +8,18 @@ class User {
   // user's email
   email: string;
 
+  // user's phone number
+  phone: string;
+
   // user's address
   address?: string;
-
-  // user's phone number
-  phone?: string;
 
   constructor(
     id: string,
     name: string,
     email: string,
-    address: string,
     phone: string,
+    address?: string,
   ) {
     this.id = id;
     this.name = name;
@@ -33,8 +33,8 @@ class User {
       id: this.id,
       name: this.name,
       email: this.email,
-      address: this.address,
       phone: this.phone,
+      address: this.address,
     };
   }
 }

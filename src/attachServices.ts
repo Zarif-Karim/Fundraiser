bb65971c59754f09263bb17b42d1918131ba3714
Zarif Karim <zarifkarim09@gmail.com>
2024-02-10 00:44:13 +1100

    implement context user service

--- src/attachServices.ts
+++ src/attachServices.ts
@@ -1,10 +1,13 @@
 import Koa from 'koa';
 import { UserService } from './services/user';
 import { ExtentedContext } from './context';
+import { RedisStorage } from './storage/redis';
+import { UserStore } from './storage/user';
 
-export default function attachServices(app: Koa): void {
+export default function attachServices(app: Koa) {
     const context = app.context as ExtentedContext;
 
-    // TODO: implement the services storage
-    app.context.userServices = new UserService({});
+    const redisStorage = new RedisStorage();
+    const userStore = new UserStore(redisStorage);
+    context.userServices = new UserService(userStore);
 }

16a017ad2d90a02cfca72d0ff3ec194023fa7d04
Zarif Karim <zarifkarim09@gmail.com>
2024-02-10 01:38:24 +1100

    fix context typo

--- src/attachServices.ts
+++ src/attachServices.ts
@@ -1,13 +1,13 @@
 import Koa from 'koa';
 import { UserService } from './services/user';
-import { ExtentedContext } from './context';
+import { ExtendedContext } from './context';
 import { RedisStorage } from './storage/redis';
 import { UserStore } from './storage/user';
 
 export default function attachServices(app: Koa) {
-    const context = app.context as ExtentedContext;
+    const context = app.context as ExtendedContext;
 
     const redisStorage = new RedisStorage();
     const userStore = new UserStore(redisStorage);
-    context.userServices = new UserService(userStore);
+    context.userService = new UserService(userStore);
 }
--- src/context.ts
+++ src/context.ts
@@ -1,6 +1,6 @@
 import { Context } from "koa";
 import { UserService } from "./services/user";
 
-export interface ExtentedContext extends Context {
-    userServices: UserService;
+export interface ExtendedContext extends Context {
+    userService: UserService;
 }

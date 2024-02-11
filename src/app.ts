7453c111f77b7fb592396626ca8cc46dd6dd3240
Zarif Karim <zarifkarim09@gmail.com>
2024-02-11 16:49:17 +1100

    refactor app to log from context

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,9 @@ import logger from 'koa-logger';
 import config from './config';
 import routes from './routes';
 import attachServices from './attachServices';
+import { ExtendedContext } from './context';
 
-const app = new Koa();
+const app = new Koa<unknown, ExtendedContext>();
 const PORT = config.PORT;
 
 app.use(bodyParser());
@@ -19,10 +20,10 @@ app.use(routes.routes());
 
 const server = app
   .listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    app.context.logger.log(`Server is running at http://localhost:${PORT}`);
   })
   .on('error', (err) => {
-    console.error(err);
+    app.context.logger.error(err);
   });
 
 export default server;

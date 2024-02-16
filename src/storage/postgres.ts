6abe800f9c2a5b8b507c1f10793b94d314d329f0
Zarif Karim <zarifkarim09@gmail.com>
2024-02-17 00:47:52 +1100

    add postgres client scaffolding

---
+++ src/storage/postgres.ts
@@ -0,0 +1,76 @@
+import type { IStorage } from './types';
+import { type DatabasePool, createPool, sql } from 'slonik';
+import type { IPostgresConfig } from '../config';
+import { Logger } from '../utils/logger';
+
+// export class PostgresClient implements IStorage {
+export class PostgresClient {
+    private pool!: DatabasePool;
+    constructor(
+        private config: IPostgresConfig,
+        private logger: Logger = console,
+    ) {}
+
+    async connect() {
+        if (this.pool) return;
+
+        this.logger.info('[POSTGRES]: Starting connection');
+        const { host, port, username, password, database } = this.config;
+        const connectionString = `postgresql://${username}:${password}@${host}:${port}/${database}`;
+        this.pool = await createPool(connectionString);
+        this.logger.info('[POSTGRES]: Connection established');
+    }
+
+    async get(query: string, params: any[] = []): Promise<any> {
+        await this.connect();
+
+        try {
+            const result = await this.pool.one(sql.unsafe`${query}`, params);
+            return result;
+        } catch (error) {
+            this.logger.error({
+                error,
+                operation: 'PostgresClient.get',
+            });
+            return {};
+        }
+    }
+
+    async add(query: string, params: any[] = []): Promise<boolean> {
+        await this.connect();
+
+        try {
+            await this.pool.query(sql.unsafe`${query}`, params);
+            return true;
+        } catch (error) {
+            this.logger.error({
+                error,
+                operation: 'PostgresClient.add',
+            });
+            return false;
+        }
+    }
+
+    async update(query: string, params: any[] = []): Promise<boolean> {
+        await this.connect();
+
+        try {
+            await this.pool.query(sql.unsafe`${query}`, params);
+            return true;
+        } catch (error) {
+            this.logger.error({
+                error,
+                operation: 'PostgresClient.update',
+            });
+            return false;
+        }
+    }
+
+    async disconnect() {
+        if (!this.pool) return;
+
+        this.logger.info('[POSTGRES]: Disconnect');
+        await this.pool.end();
+        this.logger.info('[POSTGRES]: Disconnected');
+    }
+}
